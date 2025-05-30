295b7fd50c11bfe85a333ee429c8d4d9066e3f67
Adilzhan <adilzhan1210@gmail.com>
2025-05-30 16:22:14 +0500

    Created Spring project with Spring initializr

---
+++ scouthub/src/main/java/com/scouthub/ScouthubApplication.java
@@ -0,0 +1,13 @@
+package com.scouthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScouthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScouthubApplication.class, args);
+	}
+
+}
---
+++ scouthub/src/test/java/com/scouthub/ScouthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scouthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScouthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
